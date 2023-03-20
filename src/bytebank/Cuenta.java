8115ded9b147c5fac52bb0c052530ec1a56de39e
teshy18 <sebas.tesitore@gmail.com>
2023-03-20 02:39:25 -0300

    ByteBank Alura

--- src/bytebank/Cuenta.java
+++ src/bytebank/Cuenta.java
@@ -1,9 +1,67 @@
 package bytebank;
 
 public class Cuenta {
-	double saldo;
-	int agencia;
-	int numero;
+	private double saldo;
+	private int agencia;
+	private int numero;
 	String titular;
+	private static int total = 0;
+	
+	public  static int getTotal() {
+		return total;
+	}
+	
+	public Cuenta(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("El numero de agencia no puede ser menor o igual a 0");
+		} else {
+			this.agencia = agencia;
+			total ++;
+		}
+	
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public void depositar(double valor) {
+		this.saldo = this.saldo + valor;
+	}
+	
+	public boolean retirar(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			return true;
+		}
+			return false;
+		
+	}
+	
+	public boolean transferir(double valor, Cuenta cuenta){
+		if(this.saldo >= valor) {
+			this.saldo = this.saldo -valor;
+			cuenta.depositar(valor);
+			return true;
+		} 
+			return false;
+		
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	
+	
+	
 }
 
